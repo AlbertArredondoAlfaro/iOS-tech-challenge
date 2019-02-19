05eddc1e33d5643e6d9678e6baa0535877a29b9b
Ricardo Casanova <ricardo@pijp.eu>
2019-02-19 21:53:22 +0100

    Add Reachability Manager class + bridge header file

---
+++ ABA Music/ABA-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  ABA-Bridging-Header.h
+//  ABA Music
+//
+//  Created by Ricardo Casanova on 19/02/2019.
+//  Copyright © 2019 ABA English. All rights reserved.
+//
+
+#ifndef ABA_Bridging_Header_h
+#define ABA_Bridging_Header_h
+
+//__ Reachability
+#import "Reachability.h"
+
+#endif /* ABA_Bridging_Header_h */

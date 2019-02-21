3e7148f117ce3cd729ac23044b585bb866e855e5
Ricardo Casanova <ricardo@pijp.eu>
2019-02-21 17:05:26 +0100

    Remove unused properties for TrackResponse struct

--- ABA Music/Network/Request/Data Models/Track/Track.swift
+++ ABA Music/Network/Request/Data Models/Track/Track.swift
@@ -17,35 +17,12 @@ public struct TracksResponse: Decodable {
 
 public struct TrackResponse: Decodable {
     
-    let wrapperType: String
-    let kind: String
-    let artistId: UInt
-    let collectionId: UInt?
-    let trackId: UInt
     let artistName: String
-    let collectionName: String?
     let trackName: String
-    let collectionCensoredName: String?
-    let trackCensoredName: String
-    let artistViewUrl: String
-    let collectionViewUrl: String?
     let trackViewUrl: String
     let previewUrl: String?
-    let artworkUrl30: String
-    let artworkUrl60: String
     let artworkUrl100: String
-    let collectionPrice: Float?
-    let trackPrice: Float?
     let releaseDate: String
-    let collectionExplicitness: String?
-    let trackExplicitness: String?
-    let discCount: UInt?
-    let discNumber: UInt?
-    let trackCount: UInt?
-    let trackNumber: UInt?
-    let trackTimeMillis: UInt?
-    let country: String
-    let currency: String
     let primaryGenreName: String
     
 }

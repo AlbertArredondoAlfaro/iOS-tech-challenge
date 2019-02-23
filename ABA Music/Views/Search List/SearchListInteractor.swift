3d3229f6a6e691b7de3d4739b59d78e84c814b00
Ricardo Casanova <ricardo@pijp.eu>
2019-02-23 21:18:52 +0100

    Update comments

--- ABA Music/Views/Search List/SearchListInteractor.swift
+++ ABA Music/Views/Search List/SearchListInteractor.swift
@@ -31,6 +31,14 @@ class SearchListInteractor {
 // MARK: - Private section
 extension SearchListInteractor {
     
+    /**
+     * Get tracks
+     *
+     * - parameters:
+     *      -search: the search term for tracks
+     *      -simulatedJSONFile: boolean to determinate if we need to load the info using a local json file
+     *      -completion: completion block
+     */
     private func getTracks(search: String? = nil, simulatedJSONFile: String? = nil, completion: @escaping getArtistsCompletionBlock) {
         var tracksRequest = TracksRequest(search: search)
         
@@ -39,16 +47,31 @@ extension SearchListInteractor {
         requestManager.send(request: tracksRequest)
     }
     
+    /**
+     * Update tracks results
+     *
+     * - parameters:
+     *      -tracks: tracks array with the response
+     */
     private func updateTracksWith(_ tracks: [TrackResponse]) {
         let tracksViewModel = TrackViewModel.getViewModelsWith(tracks)
         self.tracksViewModel.append(contentsOf: tracksViewModel)
     }
     
+    /**
+     * Save search (in order to add a new suggestion)
+     *
+     * - parameters:
+     *      -search: the search term
+     */
     private func saveSearch(_ search: String?) {
         guard let search = search else { return }
         SearchSuggestionsManager.shared.saveSuggestion(search)
     }
     
+    /**
+     * Get random search (for the first app running)
+     */
     private func getRandomSearch() -> String {
         let searchArray = ["The Beatles", "Oasis", "Blur", "The Verve"]
         guard let randomSearch = searchArray.randomElement() else {
@@ -63,6 +86,13 @@ extension SearchListInteractor {
 // MARK: - SearchListInteractorDelegate
 extension SearchListInteractor: SearchListInteractorDelegate {
     
+    /**
+     * Get tracks list
+     *
+     * - parameters:
+     *      -search: the search term for tracks
+     *      -completion: completion block
+     */
     func getTracksList(search: String?, completion: @escaping TracksGetTracksCompletionBlock) {
         getTracks(search: search) { [weak self] (response) in
             guard let `self` = self else { return }
@@ -85,10 +115,20 @@ extension SearchListInteractor: SearchListInteractorDelegate {
         }
     }
     
+    /**
+     * Clear current tracks information (reset)
+     */
     func clear() {
         tracksViewModel = []
     }
     
+    /**
+     * Get track view model selected at section / index
+     *
+     * - parameters:
+     *      -section: the selected section
+     *      -index: the selected index
+     */
     func getTrackSelectedAt(section: Int, index: Int) -> TrackViewModel? {
         let dictionary = Dictionary(grouping: tracksViewModel, by: { $0.artistName })
         let keysArray = Array(dictionary.keys).sorted(by: { $0 < $1 })
@@ -102,23 +142,40 @@ extension SearchListInteractor: SearchListInteractorDelegate {
         return tracks[index]
     }
     
+    /**
+     * Get all suggestions
+     *
+     * - parameters:
+     *      -completion: the completion block
+     */
     func getAllSuggestions(completion: @escaping TrackListGetSuggestionsCompletionBlock) {
         let allSuggestions = SearchSuggestionsManager.shared.getSuggestions()
         suggestions = SuggestionViewModel.getViewModelsWith(suggestions: allSuggestions)
         completion(suggestions)
     }
     
+    /**
+     * Get suggestion view model at index
+     *
+     * - parameters:
+     *      -index: the selected index
+     */
     func getSuggestionAt(index: Int) -> SuggestionViewModel? {
         if !suggestions.indices.contains(index) { return nil }
         
         return suggestions[index]
     }
     
+    /**
+     * Get the initial search term (for the initial app running)
+     */
     func getInitialSearch() -> String {
+        //__ if we don't have suggestions -> get a random search term
         guard let lastSuggestion = SearchSuggestionsManager.shared.getLastSuggestion() else {
             return getRandomSearch()
         }
         
+        //__ otherwise -> get the mos recent search term
         return lastSuggestion.suggestion
     }
     
